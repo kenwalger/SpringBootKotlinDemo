e16ee26cb2db545a2cf1e6fdf026e253d701df3c
kenwalger <kenalger@comcast.net>
2023-01-08 13:39:31 -0800

    Added database support for project.
    H2 database.

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,11 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @SpringBootApplication
 class DemoApplication
@@ -13,8 +15,38 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-@RestController
-class MessageController {
-	@GetMapping("/")
-	fun index(@RequestParam("name") name: String) = "Hello, $name!"
+data class Message(val id: String?, val text: String)
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+
+	fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun save(message: Message) {
+		val id = message.id ?: UUID.randomUUID().toString()
+		db.update("insert into messages values ( ?, ? )",
+			id, message.text)
+	}
 }
+
+@RestController
+class MessageController(val service: MessageService) {
+	@GetMapping("/")
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> =
+		service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
+}
+
